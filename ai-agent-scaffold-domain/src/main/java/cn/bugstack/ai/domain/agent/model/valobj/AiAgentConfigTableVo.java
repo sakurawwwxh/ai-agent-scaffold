455ba9f082e282fe4a12f038052e7be97db0163f
tomato <1950277291@qq.com>
2026-05-02 16:10:41 +0800

    feat: 2-3-ai-agent-config-table

---
+++ ai-agent-scaffold-domain/src/main/java/cn/bugstack/ai/domain/agent/model/valobj/AiAgentConfigTableVo.java
@@ -0,0 +1,129 @@
+package cn.bugstack.ai.domain.agent.model.valobj;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Wxh
+ * @date 2026年05月02日 9:57
+ */
+@Data
+public class AiAgentConfigTableVo {
+
+    private String appName;
+
+    private Agent agent;
+
+    private Module module;
+
+    private Map<String, String> tables;
+
+    @Data
+    public static class Agent {
+        private String agentId;
+        private String agentName;
+        private String agentDesc;
+    }
+
+    @Data
+    public static class Module{
+
+        private AiApi aiApi;
+
+        private ChatModel chatModel;
+
+        private List<Agent> agents;
+
+        private List<AgentWorkflow> agentWorkflows;
+
+        @Data
+        public static class AiApi {
+            private String baseUrl;
+            private String apiKey;
+            private String completionsPath = "/v1/chat/completions";
+            private String embeddingsPath = "/v1/embeddings";
+
+        }
+
+        @Data
+        public static class ChatModel {
+
+            private String model;
+
+            private List<ToolMcp> toolMcpList;
+
+
+            @Data
+            public static class ToolMcp {
+
+                private SSEServerParameters sse;
+
+                private StdioServerParameters stdio;
+
+                private LocalParameters local;
+
+                @Data
+                public static class SSEServerParameters {
+                    private String name;
+                    private String baseUri;
+                    private String sseEndpoint;
+                    private Integer requestTimeout = 3000;
+
+                }
+
+                @Data
+                public static class StdioServerParameters {
+                    private String name;
+                    private Integer requestTimeout = 3000;
+                    private ServerParameters serverParameters;
+
+                    @Data
+                    public static class ServerParameters {
+                        private String command;
+                        private List<String> args;
+                        private Map<String, String> env;
+
+                    }
+                }
+
+                @Data
+                public static class LocalParameters {
+                    private String name;
+                }
+
+            }
+
+        }
+
+        @Data
+        public static class Agent {
+            private String name;
+            private String instruction;
+            private String description;
+            private String outputKey;
+        }
+
+
+        @Data
+        public static class AgentWorkflow {
+            /**
+             * 类型；loop、parallel、sequential
+             */
+            private String type;
+            private String name;
+            private List<String> subAgents;
+            private String description;
+            private Integer maxIterations = 3;
+
+        }
+
+        @Data
+        public static class Runner {
+            private String agentName;
+            private List<String> pluginNameList;
+        }
+    }
+
+}
---
+++ ai-agent-scaffold-domain/src/main/java/cn/bugstack/ai/domain/agent/model/valobj/properties/AiAgentAutoConfigProperties.java
@@ -0,0 +1,21 @@
+package cn.bugstack.ai.domain.agent.model.valobj.properties;
+
+import cn.bugstack.ai.domain.agent.model.valobj.AiAgentConfigTableVo;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+/**
+ * @author Wxh
+ * @date 2026年05月02日 10:57
+ */
+
+@Data
+@ConfigurationProperties(prefix = "ai.agent.config", ignoreInvalidFields = true)
+public class AiAgentAutoConfigProperties {
+
+    private boolean enabled = false;
+
+    private Map<String, AiAgentConfigTableVo> tables;
+}
