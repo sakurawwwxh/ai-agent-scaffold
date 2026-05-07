dd757011602ac7b98e4956f3b5108ac663d09d4e
tomato <1950277291@qq.com>
2026-05-07 17:40:06 +0800

    feat:2-16-fix-多模态能力使用

--- ai-agent-scaffold-app/src/test/java/cn/bugstack/ai/test/ApiTest.java
+++ ai-agent-scaffold-app/src/test/java/cn/bugstack/ai/test/ApiTest.java
@@ -1,19 +1,100 @@
 package cn.bugstack.ai.test;
+import cn.bugstack.ai.domain.agent.service.armory.matter.patch.MySpringAI;
+import com.alibaba.fastjson.JSON;
+import com.google.adk.agents.LlmAgent;
+import com.google.adk.events.Event;
+import com.google.adk.models.springai.SpringAI;
+import com.google.adk.runner.InMemoryRunner;
+import com.google.adk.sessions.Session;
+import com.google.genai.types.Content;
+import com.google.genai.types.Part;
+import io.reactivex.rxjava3.core.Flowable;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.MimeTypeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.InputStream;
+
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
 
-    @Test
-    public void test() {
-        log.info("测试完成");
+    public static void main(String[] args) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream resourceAsStream = classLoader.getResourceAsStream("dog.png");
+        Resource resource = new ClassPathResource("dog.png", classLoader);
+        assert resourceAsStream != null;
+
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .baseUrl("https://token-plan-cn.xiaomimimo.com")
+                .apiKey("tp-ca9aehqfzwl9pc2iudxasxqbjb6o1chf70lbuuh4vwsiibw5")
+                .completionsPath("v1/chat/completions")
+                .embeddingsPath("v1/embeddings")
+                .build();
+
+        ChatModel chatModel = OpenAiChatModel.builder()
+                .openAiApi(openAiApi)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("mimo-v2.5")
+                        .build())
+                .build();
+
+        // 模型测试，没问题可以识别图片
+//        ChatResponse response = chatModel.call(new Prompt(
+//                UserMessage.builder()
+//                        .text("请描述这张图片的主要内容，并说明图中物品的可能用途。")
+//                        .media(Media.builder()
+//                                .mimeType(MimeTypeUtils.IMAGE_PNG)
+//                                .data(resource)
+//                                .build())
+//                        .build(),
+//                OpenAiChatOptions.builder()
+//                        .model("mimo-v2.5-pro")
+//                        .build()));
+//
+//        System.out.println("测试结果" + JSON.toJSONString(response));
+
+
+        // agent 测试
+        LlmAgent agent = LlmAgent.builder()
+                .name("test")
+                .description("Chess coach agent")
+                .model(new MySpringAI(chatModel))
+                .instruction("""
+                        You are a knowledgeable chess coach
+                        who helps chess players train and sharpen their chess skills.
+                        """)
+                .build();
+
+        InMemoryRunner runner = new InMemoryRunner(agent);
+
+        Session session = runner
+                .sessionService()
+                .createSession("test", "fzw")
+                .blockingGet();
+
+        Flowable<Event> events = runner.runAsync("fzw", session.id(),
+                Content.fromParts(Part.fromText("这是什么图片"),
+                        Part.fromBytes(resource.getContentAsByteArray(), MimeTypeUtils.IMAGE_PNG_VALUE)));
+
+        System.out.print("\nAgent > ");
+        events.blockingForEach(event -> System.out.println(event.stringifyContent()));
+
     }
 
 }
